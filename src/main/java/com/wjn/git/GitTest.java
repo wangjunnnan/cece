3727e3517536920c45319a955dba3e152c758a90
Junnan <Junnan@123.com>
2022-09-27 15:27:34 +0800

    first commit

---
+++ src/main/java/com/wjn/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.wjn.git;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
