dcc1d70d60be9af14cb3322746d2c765b139dfdb
Junnan <Junnan@123.com>
2022-09-27 15:29:24 +0800

    second commit

--- src/main/java/com/wjn/git/GitTest.java
+++ src/main/java/com/wjn/git/GitTest.java
@@ -5,5 +5,6 @@ import org.omg.Messaging.SyncScopeHelper;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
