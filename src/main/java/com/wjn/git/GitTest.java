7be0d2de5801359f41d543be34b7d5204b2a0e8e
wangjunnnan <114052284+wangjunnnan@users.noreply.github.com>
2022-09-27 17:40:39 +0800

    pull commit

--- src/main/java/com/wjn/git/GitTest.java
+++ src/main/java/com/wjn/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hot-fix");
         System.out.println("hello push");
+        System.out.println("hello pull");
     }
 }
